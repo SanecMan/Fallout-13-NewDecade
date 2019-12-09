1930bc29cec12fa3c97f08198e17fc2cc93a2c2b
Lomov <SeasoneWolf@yandex.ru>
2019-12-09 13:14:28 +0300

    npm v2

--- tools/travis/install_build_tools.sh
+++ tools/travis/install_build_tools.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$BUILD_TOOLS" = true ]; then
-	  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+	  apt-get install --y --yes npm
       npm install -g gulp-cli
       pip install --user PyYaml -q
       pip install --user beautifulsoup4 -q
