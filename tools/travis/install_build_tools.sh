f294c9075ddfef0c36ffbaebcfb1344ea2db1c17
Lomov <SeasoneWolf@yandex.ru>
2019-12-09 13:18:40 +0300

    npm v3

--- tools/travis/install_build_tools.sh
+++ tools/travis/install_build_tools.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$BUILD_TOOLS" = true ]; then
-	  apt-get install --y --yes npm
+	  apt-get install -y --yes npm
       npm install -g gulp-cli
       pip install --user PyYaml -q
       pip install --user beautifulsoup4 -q
