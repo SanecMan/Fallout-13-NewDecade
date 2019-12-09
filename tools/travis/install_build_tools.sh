49e47452014bf91fbec68653e8178c1513d4565f
Lomov <SeasoneWolf@yandex.ru>
2019-12-09 13:21:08 +0300

    NodeJS part2

--- tools/travis/install_build_tools.sh
+++ tools/travis/install_build_tools.sh
@@ -2,8 +2,6 @@
 set -e
 
 if [ "$BUILD_TOOLS" = true ]; then
-	  apt-get install -y --yes npm
-      npm install -g gulp-cli
       pip install --user PyYaml -q
       pip install --user beautifulsoup4 -q
 fi;
