0e61e4625c4103b3a6359209573e877049bc35ac
Lomov <SeasoneWolf@yandex.ru>
2019-12-09 13:09:35 +0300

    npm

--- tools/travis/install_build_tools.sh
+++ tools/travis/install_build_tools.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$BUILD_TOOLS" = true ]; then
-      rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh && nvm install $NODE_VERSION
+	  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
       npm install -g gulp-cli
       pip install --user PyYaml -q
       pip install --user beautifulsoup4 -q
